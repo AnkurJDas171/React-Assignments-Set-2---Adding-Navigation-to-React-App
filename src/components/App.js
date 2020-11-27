2181f538077e4a0cbae37e0ff314e1ffacb71b88
Ankurjyoti Das <68739425+AnkurJDas171@users.noreply.github.com>
2020-11-28 01:37:33 +0530

    changes made

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,39 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { useState } from "react";
+import { Link, Redirect, Route, Switch, useLocation } from "react-router-dom";
+import About from "./about";
+import Home from "./home";
+import LocationDisplay from "./LocationDisplay";
+import NoMatch from "./noMatch";
+import "../styles/App.css";
 
-class App extends Component {
-    render() {
+function App() {
+  const locationPath = useLocation();
+  const [location, setLocation] = useState(locationPath.pathname);
 
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+  const handelAncherTag = () => {
+    //let newLocation = useLocation();
+    setLocation(locationPath.pathname);
+  };
+
+  return (
+    <div id="main">
+      {/* Do not remove the main div */}
+      <LocationDisplay location={location} />
+      <Switch>
+        <Route path="/no-match" component={NoMatch} />
+        <Route path="/about" exact component={About} />
+        <Route path="/" exact component={Home} />
+        <Redirect to="/no-match" />
+      </Switch>
+
+      <a href="/" onClick={handelAncherTag}>
+        Home
+      </a>
+      <a href="/about" onClick={handelAncherTag}>
+        About
+      </a>
+    </div>
+  );
 }
-
 
 export default App;
---
+++ src/components/LocationDisplay.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function LocationDisplay({location}){
+
+  return <div data-testid="location-display">{location}</div>
+
+}
